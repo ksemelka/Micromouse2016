78757e4437aeaeafe5fc18191ab27710043c0110
ksemelka <kylesemelka@gmail.com>
2016-03-25 23:44:51 -0700

    Implemented constructor and SetTunings()

--- PID.ino
+++ PID.ino
@@ -1,15 +1,22 @@
 #include "PID.h"
 
-PID::PID(double*, double*, double*, double, double, double) {
+PID::PID(double* Input, double* Output, double* Setpoint, double Kp, double Ki, double Kd) {
+  myOutput = Output;
+  myInput = Input;
+  mySetpoint = Setpoint;
 
+  PID::SetOutputLimits(0, 255);
+  PID::SetTunings(Kp, Ki, Kd);
+
+  lastTime = millis() - SampleTime;
 }
 
 bool PID::Compute() {
 
 }
 
-void SetOutputLimits(double, double) {
-  
+void PID::SetOutputLimits(double, double) {
+
 }
 
 double PID::GetKp() const {
@@ -26,4 +33,11 @@ double PID::GetKd() const {
 
 void PID::Initialize() {
 
+}
+
+void PID::SetTunings(double Kp, double Ki, double Kd) {
+  double SampleTimeInSec = SampleTime / 1000;
+  kp = Kp;
+  ki = Ki * SampleTimeInSec;
+  kd = Kd / SampleTimeInSec;
 }
