6a9a5e6cf2c1283f2221a788b84fe67a1bc0e524
ksemelka <kylesemelka@gmail.com>
2016-03-25 21:50:57 -0700

    Added PID class and some display functions

--- PID.h
+++ PID.h
@@ -1,4 +1,21 @@
 #ifndef PID_H
 #define PID_H
 
+class PID {
+  public:
+  //Constants used in some of the functions below
+    #define AUTOMATIC 1
+    #define MANUAL  0
+    #define DIRECT  0
+    #define REVERSE  1
+
+    double GetKp() const;						  // These functions query the pid for interal values.
+    double GetKi() const;
+    double GetKd() const;
+    int GetMode() const;
+    int GetDirection() const;
+  private:
+
+};
+
 #endif /*PID_H*/
