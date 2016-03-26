bcbb0449ed4988264db11aeb3f2548b1d2dfd014
ksemelka <kylesemelka@gmail.com>
2016-03-25 23:43:48 -0700

    Cleaned up comments and formatting

--- PID.h
+++ PID.h
@@ -17,19 +17,21 @@ class PID {
     double GetKd() const;
   private:
     void Initialize();
-    double kp;
-    double ki;
-    double kd;
+    void SetTunings(double, double, double);  // Used in constructor
 
-    double *myInput;              // * Pointers to the Input, Output, and Setpoint variables.
-    double *myOutput;             //   This creates a hard link between the variables and the
-    double *mySetpoint;           //   PID, freeing the user from having to constantly tell us
+    double kp;        // Proportional tuning value
+    double ki;        // Integral tuning value
+    double kd;        // Derivative tuning value
+
+    double* myInput;              // * Pointers to the Input, Output, and Setpoint variables.
+    double* myOutput;             //   This creates a hard link between the variables and the
+    double* mySetpoint;           //   PID, freeing the user from having to constantly tell us
                                   //   what these values are. With pointers we'll just know.
 
     unsigned long lastTime;
     double ITerm, lastInput;
-    unsigned int SampleTime = 100;
     double outMin, outMax;
+    const double SampleTime = 100.0;
 };
 
 #endif /*PID_H*/
