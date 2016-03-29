bc38e35b8931feb62b29fb88889e4ad4bb5afc76
Kyle Semelka <kylesemelka@gmail.com>
2016-03-28 21:37:11 -0700

    Update Motors.h

--- Motors.h
+++ Motors.h
@@ -16,22 +16,22 @@ class Motors {
     void rotateCW();
 
   private:
-    const byte LEFTMotorEN = 5;
-    const byte LEFTlogic1 = 6;
-    const byte LEFTlogic2 = 7;
+    const byte LEFTMotorEN = 7;
+    const byte LEFTlogic1 = 22;
+    const byte LEFTlogic2 = 23;
 
-    const byte RIGHTMotorEN = 2;
-    const byte RIGHTlogic1 = 3;
-    const byte RIGHTlogic2 = 4;
+    const byte RIGHTMotorEN = 6;
+    const byte RIGHTlogic1 = 9;
+    const byte RIGHTlogic2 = 10;
 
-    const byte encoderLEFT_A = A4;  // Left encoder output channel
-    const byte encoderLEFT_B = A3;  // Left encoder output B
+    const byte encoderLEFT_A = 2;  // Left encoder output channel
+    const byte encoderLEFT_B = 3;  // Left encoder output B
     // int LEFTPos = 0;
     // int LEFT_PinALast = LOW;
     // int left = LOW;
 
-    const byte encoderRIGHT_A = A6; // Right encoder output channel
-    const byte encoderRIGHT_B = A5; // Right encoder Output B
+    const byte encoderRIGHT_A = 4; // Right encoder output channel
+    const byte encoderRIGHT_B = 5; // Right encoder Output B
     // int RIGHTPos = 0;
     // int RIGHT_PinALast = LOW;
     // int right = LOW;
