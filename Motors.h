831fc4299d44a379b39e5cd3347ef7c56cad236c
ksemelka <kylesemelka@gmail.com>
2016-03-29 00:57:33 -0700

    Added attachInterrupt

--- Motors.h
+++ Motors.h
@@ -7,34 +7,29 @@ class Motors {
     Motors();
     void halt();
     // void coast();
-    // void break();
+    void brake();
     void goForward();
     void goBackward();
     void turnLeft();
     void turnRight();
     void rotateCCW();
     void rotateCW();
+    void readEncoders();
 
   private:
-    const byte LEFTMotorEN = 5;
-    const byte LEFTlogic1 = 6;
-    const byte LEFTlogic2 = 7;
+    const byte LEFTMotorEN = 6;
+    const byte LEFTlogic1 = 22;
+    const byte LEFTlogic2 = 23;
 
-    const byte RIGHTMotorEN = 2;
-    const byte RIGHTlogic1 = 3;
-    const byte RIGHTlogic2 = 4;
+    const byte RIGHTMotorEN = 7;
+    const byte RIGHTlogic1 = 9;
+    const byte RIGHTlogic2 = 10;
 
-    const byte encoderLEFT_A = A4;  // Left encoder output channel
-    const byte encoderLEFT_B = A3;  // Left encoder output B
-    // int LEFTPos = 0;
-    // int LEFT_PinALast = LOW;
-    // int left = LOW;
+    const byte encoderLEFT_A = 2;  // Left encoder output channel
+    const byte encoderLEFT_B = 3;  // Left encoder output B
 
-    const byte encoderRIGHT_A = A6; // Right encoder output channel
-    const byte encoderRIGHT_B = A5; // Right encoder Output B
-    // int RIGHTPos = 0;
-    // int RIGHT_PinALast = LOW;
-    // int right = LOW;
+    const byte encoderRIGHT_A = 4; // Right encoder output channel
+    const byte encoderRIGHT_B = 5; // Right encoder Output B
 };
 
 #endif  /*MOTORS_H*/
