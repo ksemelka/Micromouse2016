48f70fc5a805ef49ad74d48fac070da37abcfa70
ksemelka <kylesemelka@gmail.com>
2016-03-29 01:03:17 -0700

    Added interrupt for Left encoder

--- Motors.h
+++ Motors.h
@@ -27,15 +27,9 @@ class Motors {
 
     const byte encoderLEFT_A = 2;  // Left encoder output channel
     const byte encoderLEFT_B = 3;  // Left encoder output B
-    // int LEFTPos = 0;
-    // int LEFT_PinALast = LOW;
-    // int left = LOW;
 
     const byte encoderRIGHT_A = 4; // Right encoder output channel
     const byte encoderRIGHT_B = 5; // Right encoder Output B
-    // int RIGHTPos = 0;
-    // int RIGHT_PinALast = LOW;
-    // int right = LOW;
 };
 
 #endif  /*MOTORS_H*/
