8324d3704c9e3687a169cf19047603cbe92b99c0
ksemelka <kylesemelka@gmail.com>
2016-03-29 17:37:08 -0700

    Added variables

--- main.ino
+++ main.ino
@@ -9,15 +9,19 @@
 
 volatile int RIGHT_PinALast = 0;
 volatile int encoderValue = 0;
+const byte ledPin = 13;
+extern const byte encoderLEFT_A;
+extern const byte encoderRIGHT_A;
 
 Motors motors;
 Sensors sensors(leftPT, frontPT, rightPT);
 void count(void); // code for counting the increasing values of encoder ticks
 
 void setup() {
+  pinMode(ledPin, OUTPUT);    // Initialize onboard LED
   Serial.begin(9600);
-  attachInterrupt(4, count, FALLING); // encoderRIGHT_A
-  attachInterrupt(2, count, FALLING); // encoderLEFT_A
+  attachInterrupt(encoderLEFT_A, count, FALLING);
+  attachInterrupt(encoderLEFT_A, count, FALLING);
   Serial.print("Starting...\n");
   delay(1000);
 }
