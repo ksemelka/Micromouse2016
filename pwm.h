60bac337aa3abfd627540e388468b2a4bce4ff36
ksemelka <kylesemelka@gmail.com>
2016-04-06 17:43:18 -0700

    Defined variables needed for PID

--- pwm.h
+++ pwm.h
@@ -3,6 +3,17 @@
 
 #define turnMotorOff     setLeftPwm(0);setRightPwm(0)
 
+int curSpeedX;
+int curSpeedW;
+int targetSpeedX;
+int targetSpeedW;
+int posErrorX;
+int posErrorW;
+int oldPosErrorX;
+int oldPosErrorW;
+int leftBaseSpeed;
+int rightBaseSpeed;
+
 void setLeftPwm(int);
 void setRightPwm(int);
 void calculateMotorPwm();
