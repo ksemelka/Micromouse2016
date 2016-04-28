e2e113912db57b041666d15698c1ecd0b0dfa14c
ksemelka <kylesemelka@gmail.com>
2016-04-27 23:50:54 -0700

    Moved Motor pins to #define
    No need to make it private

--- Motors.h
+++ Motors.h
@@ -8,6 +8,14 @@
 #define encoderRIGHT_B      5  // Right encoder Output B
 #define encoderTicksPerCell 1475  // Number of encoder ticks it takes to traverse cell
 
+#define RIGHTMotorEN    6
+#define RIGHTlogic1     22
+#define RIGHTlogic2     23
+
+#define LEFTMotorEN     7
+#define LEFTlogic1      9
+#define LEFTlogic2      10
+
 class Motors {
   public:
     Motors();
@@ -25,15 +33,6 @@ class Motors {
     void traverseCell();
     // void checkIfTooClose();
     // bool isTooClose();
-
-  // private:
-    const byte RIGHTMotorEN = 6;
-    const byte RIGHTlogic1 = 22;
-    const byte RIGHTlogic2 = 23;
-
-    const byte LEFTMotorEN = 7;
-    const byte LEFTlogic1 = 9;
-    const byte LEFTlogic2 = 10;
 };
 
 #endif  /*MOTORS_H*/
